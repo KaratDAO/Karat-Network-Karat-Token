7e8a701df29a161ed141603117a9b5348b524f39
haodz500 <139807747+haodz500@users.noreply.github.com>
2023-08-01 17:31:07 -0700

    Update KaratCoin.sol

--- contracts/KaratCoin.sol
+++ contracts/KaratCoin.sol
@@ -7,15 +7,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract KaratCoin is ERC20, ERC20Burnable, Ownable {
 
-    uint256 public MAX_SUPPLY = 2_000_000_000e18; // 2B KaratCoin
+    uint256 public TOTAL_SUPPLY = 2_000_000_000e18; // Two Billion Karat Coin
 
 
-    constructor(address account, uint256 initialSupply) ERC20("Karat Coin", "KAT") {
-        mint(account, initialSupply);
+    constructor(address to) ERC20("Karat Coin", "KAT") {
+        mint(to, TOTAL_SUPPLY);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {    
-        require(amount + totalSupply() <= MAX_SUPPLY, "Karat Coin: Over Supply Limit");
         _mint(to, amount);
     }
 }
