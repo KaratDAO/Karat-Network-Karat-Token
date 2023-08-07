1cd389c1ba05d77f61bc29c2a6a5f270da545840
haodz500 <139807747+haodz500@users.noreply.github.com>
2023-08-07 10:26:29 -0700

    Update KaratToken.sol

--- contracts/KaratToken.sol
+++ contracts/KaratToken.sol
@@ -3,14 +3,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KaratToken is ERC20, ERC20Burnable, Ownable {
+contract KaratToken is ERC20, ERC20Burnable {
 
     uint256 public TOTAL_SUPPLY = 2_000_000_000e18; // Two Billion Karat Token
 
     constructor(address to) ERC20("Karat Token", "KAT") {
         _mint(to, TOTAL_SUPPLY);
-        renounceOwnership();
     }
 }
