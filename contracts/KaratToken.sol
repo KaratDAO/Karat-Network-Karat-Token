483ea37d883ad6689c0ac5c8bc9a515c76a7d430
haodz500 <139807747+haodz500@users.noreply.github.com>
2023-08-06 20:52:14 -0700

    Update KaratToken.sol

--- contracts/KaratToken.sol
+++ contracts/KaratToken.sol
@@ -8,7 +8,6 @@ contract KaratToken is ERC20, ERC20Burnable, Ownable {
 
     uint256 public TOTAL_SUPPLY = 2_000_000_000e18; // Two Billion Karat Token
 
-
     constructor(address to) ERC20("Karat Token", "KAT") {
         mint(to, TOTAL_SUPPLY);
         renounceOwnership();
